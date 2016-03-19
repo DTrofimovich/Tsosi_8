6d65c83ac6dac4ab56cf74af0c42e0fdd15f5dc5
DTrofimovich <trofimovitchbob@gmail.com>
2016-03-19 12:57:44 +0300

    fixed spoiler

--- 2/lib/image.rb
+++ 2/lib/image.rb
@@ -2,9 +2,13 @@
 class Image
   attr_accessor :image, :height, :width
   def initialize(image_name)
-    image = CvMat.load(image_name, CV_LOAD_IMAGE_COLOR)
-    gray_image = image.BGR2GRAY
-    @image = gray_image.threshold(0x90, 0xFF, :binary)
+    if image_name.class == String
+      image = CvMat.load(image_name, CV_LOAD_IMAGE_COLOR)
+      gray_image = image.BGR2GRAY
+      @image = gray_image.threshold(0x90, 0xFF, :binary)
+    else
+      @image = image_name
+    end
     @height = @image.size.height
     @width = @image.size.width
   rescue
--- 2/lib/image_spoiler.rb
+++ 2/lib/image_spoiler.rb
@@ -4,18 +4,18 @@ class ImageSpoiler
   end
 
   def spoil_image(per_cent_spoil)
-    image_c = @image.clone
-    lenght = image_c.height * image_c.width
-    random = (0..lenght - 1).to_a.sample(lenght * per_cent_spoil / 100)
+    image_c = @image.image.clone
+    lenght = @image.height * @image.width
+    random = (0..(lenght - 1)).to_a.sample(lenght * per_cent_spoil / 100)
     random.each do |r|
-      if (image_c.image[r].to_a == CvColor::Black.to_a)
-        image_c.image[r] = CvColor::White
+      if (image_c[r].to_a == CvColor::Black.to_a)
+        image_c[r] = CvColor::White
       else
-        image_c.image[r] = CvColor::Black
+        image_c[r] = CvColor::Black
       end
     end
     window = GUI::Window.new("win")
-    window.show(image_c.image.resize(CvSize.new(320, 320)))
-    Pattern.new(image_c)
+    window.show(image_c.resize(CvSize.new(320, 320)))
+    Pattern.new(Image.new(image_c))
   end
 end
